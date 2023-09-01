0f5ab5d017542f2c0bdce9e6b6ab0e2bef0ee513
P-JSL <althsuwpfl@gmail.com>
2023-09-01 20:58:26 +0900

    8장 마지막

---
+++ Internationalization/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+using System.Globalization;
+
+CultureInfo globalization = CultureInfo.CurrentCulture;
+CultureInfo localization = CultureInfo.CurrentUICulture;
+WriteLine("The current globalization culture is {0}: {1}",
+    arg0:globalization,arg1:globalization.DisplayName);
---
+++ WorkingWithImages/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+string realPath = @"C:\Users\admin\source\repos\Code\Chapter08\WorkingWithImages";
+
+string imagesFolder = Path.Combine(
+    realPath, "images");
+
+IEnumerable<string> images =
+    Directory.EnumerateFiles(imagesFolder);
+
+foreach (string imagePath in images)
+{
+    string thumbnailPath = Path.Combine(realPath, "images",Path.GetFileNameWithoutExtension(imagePath) + "-섬네일" + Path.GetExtension(imagePath));
+    using(Image image = Image.Load(imagePath))
+    {
+        image.Mutate(x => x.Resize(image.Width/10, image.Height/10));
+        image.Mutate(x => x.Grayscale());
+        image.Save(thumbnailPath);
+    }
+}
+
+WriteLine("이미지 처리 완료! images 폴더를 확인하세요.");
